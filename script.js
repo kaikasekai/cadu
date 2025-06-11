01acfdd6cee2e6e56a2e9980ca3def97c19a6083
kaikasekai <114289526+kaikasekai@users.noreply.github.com>
2025-06-11 16:00:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const CSV_URL = 'https://raw.githubusercontent.com/kaikasekai/caduai/refs/heads/main/data.csv';
+const CSV_URL = 'https://raw.githubusercontent.com/kaikasekai/caduai/refs/heads/main/data.csv?token=GHSAT0AAAAAADFPDT3RTISW7RJV5HAK3TBA2CJPWCQ';
 
 async function fetchCSV(url) {
   const res = await fetch(url);
